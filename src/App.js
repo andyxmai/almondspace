c59ef9b2f0a8a8545b9079c5c1ae9ec157874f31
Andy Mai <andrew.x.mai@gmail.com>
2017-08-01 10:32:00 -0700

    redesign

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import calendar from './calendar.png'
-import coffee from './coffee.png'
+import office from './office-block.png'
 import guitar from './guitar.png'
 import waiter from './waiter.png'
 import './App.css'
@@ -24,11 +24,11 @@ class App extends Component {
                 <div>Repurposed space crafted for meetings</div>
               </div>
               <p className="hero-intro">
-                We created an experience that lets you focus solely on the people you are meeting. And nothing else.
+                We created an experience that lets you brainstorm, collaborate, and unwind with coworkers.
               </p>
             </div>
             <div className="reservation">
-              <div className="pricing">
+{/*              <div className="pricing">
                 <div className="price-amount">
                   <span className="price-dollar-sign">$</span>
                   <span>6</span>
@@ -38,23 +38,18 @@ class App extends Component {
                   minimum 30 min · prorated after first 30 min
                 </div>
               </div>
+*/}
               <div className="reserve-button">
                 <a href="sms:6503089561&body=I'd like to reserve a table!"><button type="button" className="btn btn-primary reserve-btn"><b>Reserve your table</b></button></a>
               </div>
-              <div className="reserve-text">
-                <div className="reserve-text-title">
-                  To reserve your table, text
-                </div>
-                <div>
-                  (650) 308-9561
-                </div>
-              </div>
             </div>
+
             <div className="map">
               <iframe className="map-frame" frameborder="0"
                 title="map"
                 src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJL4mjMTm7j4ARSrl1ZnMUS80&zoom=16&key=AIzaSyCDzFQUy2Ip5qA7YbLAJCK9yNd4MMZt9GE" allowfullscreen></iframe>
             </div>
+
             <div className="why-almondspace">
               <div className="section-title">Why AlmondSpace?</div>
 
@@ -62,22 +57,22 @@ class App extends Component {
                 <div className="col-sm-6">
                   <div className="icon-text perk-icon-text">
                     <div>
-                      <img src={waiter} className="perk-icon" alt="service" />
+                      <img src={calendar} className="perk-icon" alt="schedule" />
                     </div>
                     <div className="perk">
-                      <div className="perk-header">Convenience</div>
-                      <div className="perk-description">No more waiting in line to order and have awkward small talk</div>
+                      <div className="perk-header">Reservations</div>
+                      <div className="perk-description">Book your table and guarantee your seating</div>
                     </div>
                   </div>
                 </div>
                 <div className="col-sm-6">
                   <div className="icon-text perk-icon-text">
                     <div>
-                      <img src={calendar} className="perk-icon" alt="schedule" />
+                      <img src={waiter} className="perk-icon" alt="service" />
                     </div>
                     <div className="perk">
-                      <div className="perk-header">Availability</div>
-                      <div className="perk-description">Reservations are available to guarantee your seating</div>
+                      <div className="perk-header">Service</div>
+                      <div className="perk-description">Unlimited coffee and tea served while you are here</div>
                     </div>
                   </div>
                 </div>
@@ -87,27 +82,30 @@ class App extends Component {
                 <div className="col-sm-6">
                   <div className="icon-text perk-icon-text">
                     <div>
+                      <img src={office} className="perk-icon" alt="convenience" />
+                    </div>
+                    <div className="perk">
+                      <div className="perk-header">Convenience</div>
+                      <div className="perk-description">Prime locations just steps from your office</div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="col-sm-6">
+                  <div className="icon-text perk-icon-text">
+                    <div>
                       <img src={guitar} className="perk-icon" alt="ambiance" />
                     </div>
                     <div className="perk">
                       <div className="perk-header">Ambiance</div>
-                      <div className="perk-description">Our space is engineered to reduce background noise</div>
+                      <div className="perk-description">A place to meet without disrupting other coworkers</div>
                     </div>
                   </div>
                 </div>
-                <div className="col-sm-6">
-                  <div className="icon-text perk-icon-text">
-                    <div>
-                      <img src={coffee} className="perk-icon" alt="grubs" />
-                    </div>
-                    <div className="perk">
-                      <div className="perk-header">Grubs</div>
-                      <div className="perk-description">Unlimited coffee, tea, tasty snacks, and almonds!</div>
-                    </div>
-                  </div>
-                </div>
+
               </div>
             </div>
+
           </div>
         </div>
       </div>
